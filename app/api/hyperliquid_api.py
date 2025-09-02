f21c82088f6fbd6239f30ad345dc98b18aeaf311
luka8-ops <radicluka315@gmail.com>
2025-09-02 22:15:21 +0200

    Update hyperliquid_api.py

--- app/api/hyperliquid_api.py
+++ app/api/hyperliquid_api.py
@@ -17,29 +17,26 @@ def setup(base_url=None, skip_ws=False, perp_dexs=None):
     secret_key = get_secret_key()
     account: LocalAccount = eth_account.Account.from_key(secret_key)
     
-    address = account.address
-    # if not address:
-    #     address = account.address
-    #     print("Using wallet address as account address:", address)
-    # else:
-    #     print("Running with account address:", address)
+    address = settings.HYPERLIQUID_ACCOUNT_ADDRESS
+    if not address:
+        address = account.address
+        print("Using wallet address as account address:", address)
+    else:
+        print("Running with account address:", address)
         
-    # if address != account.address:
-    #     print("Running with agent address:", account.address)
+    if address != account.address:
+        print("Running with agent address:", account.address)
 
     info = Info(base_url, skip_ws, perp_dexs=perp_dexs)
     user_state = info.user_state(address)
     spot_user_state = info.spot_user_state(address)
     margin_summary = user_state["marginSummary"]
 
-    print(f"Account Value: ${margin_summary['accountValue']}")
-    print(spot_user_state)
-
     if float(margin_summary["accountValue"]) == 0 and len(spot_user_state["balances"]) == 0:
         print("Not running the example because the provided account has no equity.")
         url = info.base_url.split(".", 1)[1]
         error_string = f"No accountValue:\nIf you think this is a mistake, make sure that {address} has a balance on {url}.\nIf address shown is your API wallet address, update the config to specify the address of your account, not the address of the API wallet."
         raise Exception(error_string)
-    
-    exchange = Exchange(account, base_url, vault_address="0x712fa55530c25d2502b66e87d178809e206a354f", account_address=address, perp_dexs=perp_dexs)
+
+    exchange = Exchange(account, base_url, account_address=address, perp_dexs=perp_dexs)
     return address, info, exchange
